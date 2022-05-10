1f21c5b88616f39a5ba5a8feb20d403f20df638d
kapable <skjoldenstore@gmail.com>
2022-05-10 20:08:55 +0900

    fix css for header and nav

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Drawer, Menu, Layout } from 'antd';
+import { Drawer, Layout, Divider } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
@@ -20,32 +20,29 @@ const AppLayout = ({ children }) => {
             <Header className='applayout-header'>
                 <img className='applayout-header-main-logo' src='https://images.niair.xyz/basic/kfunny_logo.png' alt='케이퍼니' />
                 <div className='applayout-header-hamburger' onClick={onClickHamburger}><MenuOutlined /></div>
-            <Drawer
-                title="MBTI 커뮤니티"
-                placement='right'
-                closable={false}
-                onClose={onCloseDrawer}
-                visible={showDrawer}
-                key="applayout-drawer"
-                width={270}
-            >
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-            </Drawer>
+                <Drawer
+                    title="MBTI 커뮤니티"
+                    placement='right'
+                    closable={false}
+                    onClose={onCloseDrawer}
+                    visible={showDrawer}
+                    key="applayout-drawer"
+                    width={270}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Drawer>
+            </Header>
             <div className='applayout-nav'>
                 <div className='applayout-nav-signup-div'><Link href='/signup'><a className='applayout-nav-signup-div-a'>회원가입</a></Link></div>
                 <div className='applayout-nav-login-div'><Link href='/login'><a className='applayout-nav-login-div-a'>로그인</a></Link></div>
             </div>
-            {/* <Menu className='applayout-nav' mode='horizontal' triggerSubMenuAction="click" theme='light'>
-                <Menu.Item key="login"></Menu.Item>
-                <Menu.Item key="divider" disabled={true}>|</Menu.Item>
-                <Menu.Item key="signup"></Menu.Item>
-            </Menu> */}
-            </Header>
-            {/* <Content>
+            {/* <Divider /> */}
+                {/* <br style={{clear: "all"}}/> */}
+            <Content className='applayout-content'>
                 { children }
-            </Content> */}
+            </Content>
         </Layout>
     );
 };
