89603fb6981234f88326bd4b545ee04f7e7e58a2
kapable <skjoldenstore@gmail.com>
2022-05-15 22:54:54 +0900

    set profile header and menu tab

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,5 +1,8 @@
 import React, { Fragment } from 'react';
 import Head from 'next/head';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 const Profile = () => {
     return (
@@ -12,7 +15,25 @@ const Profile = () => {
                 <meta name="description" content="MBTI 커뮤니티" />
                 <meta name="keywords" content="MBTI, 커뮤니티" />
             </Head>
-            <div>Profile</div>
+            <div className='profile-head-div'>
+                <div className='profile-head-name'>웡아잉</div>
+                <div className='profile-head-email'>ellen0@gmail.com</div>
+                <div className='profile-head-type'>ENTP</div>
+            </div>
+            <Tabs className='profile-menu-tab' tabBarStyle={{margin:"0 auto", width:"fit-content"}} tabPosition='top' size='default' type='line'>
+                <TabPane tab="작성글" key="1">
+                    작성글
+                </TabPane>
+                <TabPane tab="작성댓글" key="2">
+                    작성댓글
+                </TabPane>
+                <TabPane tab="댓글단 글" key="3">
+                    댓글단 글
+                </TabPane>
+                <TabPane tab="좋아요한 글" key="4">
+                    좋아요한 글
+                </TabPane>
+            </Tabs>
         </Fragment>
     );
 };
